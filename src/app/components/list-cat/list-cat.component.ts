b091067aa2727ea09e244019255860e68992ee54
Bilkiss Dulloo <bilkiss.dulloo@gmail.com>
2019-01-31 22:22:49 +0400

    Sort by score on cat list.

--- src/app/components/list-cat/list-cat.component.ts
+++ src/app/components/list-cat/list-cat.component.ts
@@ -20,14 +20,20 @@ export class ListCatComponent implements OnInit {
 
   getListCat(){
     this.endpointService.getCats().subscribe( res => {
-      // console.log("Res: ", res);
       this.catListObj = res;
       this.catList = this.catListObj.images;
       // console.log("catList: ", this.catList);
+      if(this.catList.length >0){
+        this.sortCatByScore();
+      }
 
     }, error => {
       console.log("Error: ", error);
     })
+  }
+
+  sortCatByScore(){
+    this.catList.sort((a,b) => (a.score > b.score) ? -1 : ((b.score > a.score) ? 1 : 0));
   }
 
 }
