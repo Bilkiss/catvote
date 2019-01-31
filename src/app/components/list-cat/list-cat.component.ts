09f6294539638f2ceecbf954be6c58d4157bc916
Bilkiss Dulloo <bilkiss.dulloo@gmail.com>
2019-01-31 21:37:39 +0400

    Added bootstrap 4 & SCSS

--- src/app/components/list-cat/list-cat.component.ts
+++ src/app/components/list-cat/list-cat.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-list-cat',
   templateUrl: './list-cat.component.html',
-  styleUrls: ['./list-cat.component.css']
+  styleUrls: ['./list-cat.component.scss']
 })
 export class ListCatComponent implements OnInit {
 
